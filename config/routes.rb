93d206333b06df6190690f67725d8b2e7734379f
EC2 Default User <ootakaebisu@gmail.com>
2020-11-16 11:53:45 +0000

    [finish]

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root 'homes#top'
+  devise_for :users
+  get 'home/about' => 'homes#about', as: 'about'
+  resources :users, only: [:show,:index,:edit,:update]
+  resources :books
+
+
+end
