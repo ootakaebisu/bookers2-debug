50bdfc4976d79e777d8d4eeefdd574abbe0fd3ff
EC2 Default User <ootakaebisu@gmail.com>
2020-11-17 06:25:39 +0000

    [add]いいね機能の追加/redirect_back

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,21 @@
 class FavoritesController < ApplicationController
-  
+
   def create
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: book.id)
+    favorite.save
+    
+    # 同じベージに遷移するパス指定・相対的
+    redirect_back(fallback_location: root_path)
   end
-  
+
   def destroy
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: book.id)
+    favorite.destroy
+    
+    # 同じベージに遷移するパス指定・相対的
+    redirect_back(fallback_location: root_path)
   end
-  
+
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -5,5 +5,11 @@ class Book < ApplicationRecord
 
 	belongs_to :user
 	has_many :book_comments, dependent: :destroy
+	has_many :favorites, dependent: :destroy
+
+	# 投稿画像に付けられた「いいね」に、自分（ログインしているユーザ）が含まれているかをfavorited_by?メソッドで判定しています
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 
 end
