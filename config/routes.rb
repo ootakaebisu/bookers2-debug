9c1f51f1e64c268db01ad3d4bba7a6e6a0030c46
EC2 Default User <ootakaebisu@gmail.com>
2020-11-18 14:40:38 +0000

    [fix]follow/unfollowをcreate/destroyに修正

--- config/routes.rb
+++ config/routes.rb
@@ -16,8 +16,8 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
   end
 
-  post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
-  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
+  post 'create/:id' => 'relationships#create', as: 'follow' # フォローする
+  post 'destroy/:id' => 'relationships#destroy', as: 'unfollow' # フォロー外す
 
 
 end
