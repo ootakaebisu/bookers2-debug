35b5646185a0be34bb3d7f76d5d424712cd74fc6
EC2 Default User <ootakaebisu@gmail.com>
2020-11-22 11:54:01 +0000

    [add]いいね非同期通信実装済み/コメント非同期通信実装中

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,21 +1,23 @@
 class FavoritesController < ApplicationController
 
   def create
+    @book = Book.find(params[:book_id])
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    
-    # 同じベージに遷移するパス指定・相対的
-    redirect_back(fallback_location: root_path)
+
+    # # 同じベージに遷移するパス指定・相対的
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    
+
     # 同じベージに遷移するパス指定・相対的
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
 
 end
